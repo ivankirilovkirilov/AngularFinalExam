6de17721194954ee4b59b6d7dca70c44dfa243da
Ivan <ivankirilovkirilov123@gmail.com>
2021-08-19 17:43:08 +0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavComponent } from './components/nav/nav.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'tasks', pathMatch: 'full'},
+  {path: 'about', component: AboutComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'tasks', component: TasksComponent},
+  {path: '**', component: PageNotFoundComponent},
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TasksComponent,
+    TaskItemComponent,
+    AddTaskComponent,
+    AboutComponent,
+    RegisterComponent,
+    NavComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes, {enableTracing: true})
   ],
   providers: [],
   bootstrap: [AppComponent]
